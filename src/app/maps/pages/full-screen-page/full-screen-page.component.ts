ef83dfc0a003e58e9dad15c582ca2a40d014ad49
Álex <alejandro.mcamps@bosonit.com>
2023-09-27 11:46:51 +0200

    Creando estructura, enrutado y añadiendo mapbox

---
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  
+  templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.css']
+})
+export class FullScreenPageComponent {
+
+}
